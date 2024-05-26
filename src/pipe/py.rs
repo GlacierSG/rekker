cee1c35b60fe0020e88386698a66da8b470a3d5e
GlacierSG <glaciersg@proton.me>
2024-05-26 14:55:54 +0000

    added support for string input (python)

--- src/pipe/py.rs
+++ src/pipe/py.rs
@@ -1,6 +1,21 @@
 use pyo3::prelude::*;
-use pyo3::types::PyBytes;
 use crate::pipe::pipe::Pipe;
+use pyo3::types::{PyBytes, PyString, PyAny};
+
+fn inp_to_bytes(obj: &PyAny) -> PyResult<Vec<u8>> {
+    if obj.is_instance_of::<PyString>() {
+        let s: String = obj.extract()?;
+        Ok(s.as_bytes().to_vec())
+    } else if obj.is_instance_of::<PyBytes>() {
+        let b: Vec<u8> = obj.extract()?;
+        Ok(b)
+    } else {
+        Err(PyErr::new::<pyo3::exceptions::PyTypeError, _>(
+            "Expected a string or bytes object",
+        ))
+    }
+}
+
 
 pub fn pipes(_py: Python, m: &PyModule)  -> PyResult<()> {
     m.add_class::<Tcp>()?;
@@ -42,13 +57,17 @@ impl Tcp {
         Ok(PyBytes::new_bound(py, &out).into())
     }
 
-    fn send(&mut self, _py: Python, data: &[u8]) -> PyResult<usize> {
+    fn send(&mut self, _py: Python, data: &PyAny) -> PyResult<usize> {
+        let data = inp_to_bytes(&data)?;
         Ok(self.tcp.send(data)?)
     }
-    fn sendline(&mut self, _py: Python, data: &[u8]) -> PyResult<usize> {
+    fn sendline(&mut self, _py: Python, data: &PyAny) -> PyResult<usize> {
+        let data = inp_to_bytes(&data)?;
         Ok(self.tcp.sendline(data)?)
     }
-    fn sendlineafter(&mut self, py: Python, data: &[u8], suffix: &[u8]) -> PyResult<Py<PyBytes>> {
+    fn sendlineafter(&mut self, py: Python, data: &PyAny, suffix: &PyAny) -> PyResult<Py<PyBytes>> {
+        let data = inp_to_bytes(&data)?;
+        let suffix = inp_to_bytes(&suffix)?;
         let out = self.tcp.sendlineafter(data, suffix)?;
         Ok(PyBytes::new_bound(py, &out).into())
     }
